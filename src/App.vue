2f74a5cfb99e9145fe06abc7ca0428b58e1ca5fa
“Gui” <gui.denez56@gmail.com>
2022-10-04 13:45:55 -0300

    vuex

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ProductListOne></ProductListOne>
+    <ProductListTwo></ProductListTwo>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne.vue"
+import ProductListTwo from "./components/ProductListTwo.vue"
+export default {
+    name: "app",
+    components: {
+        ProductListOne,
+        ProductListTwo
+    },
+    data() {
+        return {};
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="product-list-one">
+        <h1>Product List One</h1>
+        <ul>
+            <li v-for="product in products" :key="product.name">
+                <span>{{product.name}}</span>
+                <span>£{{product.price}}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
