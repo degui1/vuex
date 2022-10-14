c24666c8dba45e26cf9c24f3eab4c9f173112ece
“Gui” <gui.denez56@gmail.com>
2022-10-14 11:29:40 -0300

    getters, actions, maps

--- src/store/index.js
+++ src/store/index.js
@@ -5,19 +5,51 @@ export default createStore({
   strict: true,
   state: {
     user: {
-      first_name: 'John'
-    }
+      first_name: 'John',
+      last_name: 'Snow'
+    },
+    cart: [],
   },
   getters: {
+    fullName(state) {
+      return `${state.user.first_name} ${state.user.last_name}`
+    }
   },
   mutations: {
-    save(state, payload) {
+    //as funções estão em letra maiúscula apenas por causa da convenção, mas nao muda nada. É uma forma de identificar no
+    // componente o vuex
+    SAVE(state, payload) {
       // console.log(state, payload)
       state.user.first_name = payload
+    },
+    SAVE_LAST_NAME(state, payload) {
+      state.user.last_name = payload
+    },
+    ADD_TO_CART(state, payload) {
+
+      const existingProduct = state.cart.find(o => o.id === payload.id)
+
+      if(existingProduct){
+        existingProduct.qtd += 1
+      }
+      else{
+        payload.qtd = 1
+        state.cart.push(payload);
+      }
+    },
+    REMOVE_PRODUCT(state, payload) {
+      const existingProduct = state.cart.find(o => o.id === payload.id)
+
+      if (existingProduct && existingProduct.qtd > 1) {
+        existingProduct.qtd -= 1
+      } else {
+        const id = state.cart.findIndex(o => o.id === payload.id)
+        state.cart.splice(id, 1)
+      }
     }
   },
   actions: {
-    save(context, payload) {
+    saveFirstName(context, payload) {
       context.commit('save', payload)
 
     }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="home">
-    <h1>{{$store.state.user.first_name}}</h1>
+  <div class="">
+
+    <div class="row">
+      <div class="col" v-for="product in products" :key="product.id">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+            <div class="card-body">
+              <p class="card-text">{{product.name}}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button @click="ADD_TO_CART(product)" type="button" class="btn btn-sm btn-outline-secondary">Adicionar</button>
+                  <button v-if="!!qtdProduct(product.id)" @click="REMOVE_PRODUCT(product)" type="button" class="btn btn-sm btn-outline-secondary">Remover</button>
+                </div>
+                <small v-if="!!qtdProduct(product.id)" class="text-muted">
+                  {{qtdProduct( product.id )}}
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <br>
+
+    <pre>
+      {{cart}}
+    </pre>
+
+
+
+    <br><br><br><br><br>
+    <h1>{{fullName}}</h1>
     {{text}}
-    <input type="text" v-model="text">
-    <button @click="save">Salvar</button>
+    <input type="text" v-model="name">
+    <input type="text" v-model="lastName">
+    <!-- <button @click="save">Salvar</button> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
 export default {
   name: 'HomeView',
   components: {
@@ -16,16 +49,61 @@ export default {
   data() {
     return {
       text: '',
+      products: [
+        {id: 1, name: 'Produto 1'},
+        {id: 2, name: 'Produto 2'},
+        {id: 3, name: 'Produto 3'}
+      ]
     }
   },
-  
+  computed: {
+    ...mapGetters(['fullName']),
+    ...mapState({
+      user: state => state.user,
+      cart: state => state.cart
+    }),
+
+    name: {
+      get() {
+        return this.user.first_name;
+      },
+      set(value) {
+        this.SAVE(value)
+      }
+    },
+    lastName: {
+      get() {
+        return this.user.last_name;
+      },
+      set(value) {
+        this.SAVE_LAST_NAME(value);
+      }
+    },
+    // fullName() {
+    //   return `${this.name}  ${this.lastName}`
+    // }
+  },
   methods: {
+    ...mapMutations(['ADD_TO_CART', 'REMOVE_PRODUCT', 'SAVE', 'SAVE_LAST_NAME']),
+    ...mapActions(['saveFirstName']),
+
     save() {
       // Action
-      // this.$store.dispatch('save', this.text)
+      // this.$store.dispatch('saveFirstName', this.text)
+      // COM MAPEAMENTO
+      // this.saveFirstName(this.text)
 
       //Mutation
-      this.$store.commit('save', this.text)
+      // this.$store.commit('save', this.text)
+    },
+    // saveProduct(product) {
+    //   // this.$store.commit('addToCart', product)
+    // COM MAPEAMENTO
+    //   this.addToCart(product)
+    // },
+    qtdProduct(id) {
+      // return this.$store.state.cart.find(o => o.id === id)?.qtd || 0
+      return this.cart.find(o => o.id === id)?.qtd || 0
     }
   }
 
